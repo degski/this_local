3123684fae980ba4aabd8ee32109865e25e7bc2c
degski <degski@gmail.com>
2020-05-20 07:42:49 -0500

    save

--- include/this_local/this_local/detail/this_local/detail.hpp
+++ include/this_local/this_local/detail/this_local/detail.hpp
@@ -570,36 +570,28 @@ class lock_free_plf_list {
         old_counter_->external_count = new_counter.external_count;
     }
 
-    [[nodiscard]] HEDLEY_ALWAYS_INLINE counted_link get_link ( node_ptr new_node_,
-                                                               counted_node_link prev_link_ ) noexcept { // has side-effects
-
-        counted_link new_link             = { prev_link_.prev, ( counted_link_ptr ) new_node_, 5 /* prev_link_.external_count */ };
-        ( ( counted_link & ) *new_node_ ) = { ( counted_link_ptr ) prev_link_.prev, prev_link_.next,
-                                              10 /* prev_link_.external_count */ };
-        return new_link;
-    }
-
-    HEDLEY_ALWAYS_INLINE void counted_back_store ( node_ptr p_, counted_link l_, char c_ = 0 ) noexcept {
-        std::memcpy ( reinterpret_cast<char *> ( &p_ ) + 7, &c_, 1 ); // little-endian?
+    HEDLEY_ALWAYS_INLINE void counted_back_store ( node_ptr p_, counted_link l_, char aba_id_ = 0 ) noexcept {
+        std::memcpy ( reinterpret_cast<char *> ( &p_ ) + 7, &aba_id_, 1 ); // little-endian?
         back.store ( { std::forward<counted_link> ( l_ ), std::forward<node_ptr> ( p_ ) }, std::memory_order_relaxed );
-    }
-
-    HEDLEY_ALWAYS_INLINE std::tuple<counted_link, node_ptr, char> counted_back_load ( node_ptr p_ ) noexcept {
-        counted_node_link cnl = back.load ( std::memory_order_relaxed );
-        char c                = std::exchange ( reinterpret_cast<char *> ( &cnl.node ) + 7, 0 );
-        return { std::forward<counted_link> ( counted_link::cnl ), std::forward<node_ptr> ( cnl.node ), std::forward<char> ( c ) };
     }
 
     [[maybe_unused]] HEDLEY_NEVER_INLINE nodes_iterator insert_regular_implementation ( nodes_iterator && it_ ) noexcept {
 
         node_ptr new_node = &*it_;
 
-        do {
-            node_ptr old_node                = load_old_node ( );
-            *( ( counted_link * ) new_node ) = { old_node, old_node->next };
-            counted_link new_link_old_node   = { old_node->prev, new_node };
+        counted_node_link old = back.load ( std::memory_order_relaxed );
+        char new_aba_id       = std::exchange ( *( reinterpret_cast<char> ( &old.node ) + 7 ), 0 )++;
 
-        } while ( not dwcas ( *( ( counted_link * ) old_node ), new_link_old_node, *( ( counted_link * ) new_node ) ) );
+        *( ( counted_link * ) new_node ) = { old.node, old.node->next };
+        counted_back_store ( new_node, { old.node->prev, new_node }, new_aba_id );
+
+        while ( not dwcas ( *( ( counted_link * ) old_node ), back.load ( std::memory_order_relaxed ),
+                            *( ( counted_link * ) new_node ) ) ) {
+            old = back.load ( std::memory_order_relaxed );
+            std::memset ( reinterpret_cast<char> ( &old.node ) + 7 ) , 0, 1 )
+            *( ( counted_link * ) new_node ) = { old.node, old.node->next };
+            counted_back_store ( new_node, { old.node->prev, new_node }, new_aba_id );
+        }
 
         back_store ( regular );
         new_node->next->prev = regular;
